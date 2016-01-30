308187e19440612fc4c9923ebaeeb511f5c4d93a
KentarOu <kanno@kentarou.me>
2016-01-30 17:22:50 +0900

    first commit

---
+++ NIFTY_ObjC/PushNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushNotification
+//
+//  Created by KentarOu on 2016/01/30.
+//  Copyright © 2016年 KentarOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NIFTY_ObjC/PushNotification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PushNotification
+//
+//  Created by KentarOu on 2016/01/30.
+//  Copyright © 2016年 KentarOu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
